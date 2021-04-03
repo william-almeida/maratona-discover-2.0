fc0663987e0f9461e64ae454aff5b4d5a8ea7b99
William <william.cntt@gmail.com>
2021-04-03 16:05:31 -0300

    identation size

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,8 @@ const express = require('express')
 const routes = express.Router() 
 const views = __dirname + '/views/'
 
-const profile = {
+const Profile = {
+  date: {
     name: "William-Almeida",
     avatar: `https:github.com/william-almeida.png`,
     "monthly-budget": 3500,
@@ -12,75 +13,72 @@ const profile = {
     "days-per-week": 5,
     "vocation-per-year": 2,
     "hour-value": 50
+  },
 }
 
 const Job = {
-    data: [
-        {
-            id: 1,
-            name: 'Maratona Discover',
-            "daily-hours": 3,
-            "total-hours": 40,
-            created_at: Date.now()  
-        },
-        {
-            id: 2,
-            name: 'GitHub',
-            "daily-hours": 1,
-            "total-hours": 0,
-            created_at: Date.now()  
-        }
-    ],
-    controllers: {
-        index(req, res) {
-            const updatedJobs = JOb.data.map((job) => {
-                const remaining = Job.services.remainingDays(job)
-                const status = remaining <= 0 ? 'done' : 'progress'
-                // ...job + incremento
-                return {
-                    ...job,
-                    remaining,
-                    status,
-                    budget: profile["hour-value"] * job["total-hours"]
-                }
-            })
-        
-            return res.render(views + 'index', { jobs:updatedJobs })
-        },
-        create(req, res) {
-            return res.render(views + 'job')
-        },
-        post(req, res){
-            const lastId = Job.data[Job.data.length -1]?.id  || 1
-            Job.data.push({
-                id: lastId + 1,
-                name: req.body.name,
-                "daily-hours": req.body["daily-hours"],
-                "total-hours": req.body["total-hours"],
-                created_at: Date.now()      
-            })
-            return res.redirect('/')
-        }
-
+  data: [
+    {
+      id: 1,
+      name: 'Maratona Discover',
+      "daily-hours": 3,
+      "total-hours": 40,
+      created_at: Date.now()  
     },
-    services: {
-        remainingDays(job) {
-            const remainingDays = (job['total-hours'] / job['daily-hours']).toFixed()
-            const createdDate = new Date(job.created_at)
-            const dueDay = createdDate.getDate() + Number(remainingDays)
-            const dueDateInMs = createdDate.setDate(dueDay)
-            const timeDiffInMs = dueDateInMs - Date.now()
-            // change milliseconds to days
-            const dayInMs = 1000 * 60 * 60 * 24 //86400000ms
-            const dayDiff = (timeDiffInMs / dayInMs ).toFixed()
-            
-            return dayDiff
-        }
+    {
+      id: 2,
+      name: 'GitHub',
+      "daily-hours": 1,
+      "total-hours": 0,
+      created_at: Date.now()  
     }
+  ],
+  controllers: {
+    index(req, res) {
+      const updatedJobs = JOb.data.map((job) => {
+        const remaining = Job.services.remainingDays(job)
+        const status = remaining <= 0 ? 'done' : 'progress'
+        // ...job + incremento
+        return {
+          ...job,
+          remaining,
+          status,
+          budget: profile["hour-value"] * job["total-hours"]
+        }
+      })
+      return res.render(views + 'index', { jobs:updatedJobs })
+    },
+    create(req, res) {
+      return res.render(views + 'job')
+    },
+    post(req, res){
+      const lastId = Job.data[Job.data.length -1]?.id  || 1
+      Job.data.push({
+        id: lastId + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_at: Date.now()      
+      })
+      return res.redirect('/')
+    }
+
+  },
+  services: {
+    remainingDays(job) {
+      const remainingDays = (job['total-hours'] / job['daily-hours']).toFixed()
+      const createdDate = new Date(job.created_at)
+      const dueDay = createdDate.getDate() + Number(remainingDays)
+      const dueDateInMs = createdDate.setDate(dueDay)
+      const timeDiffInMs = dueDateInMs - Date.now()
+      // change milliseconds to days
+      const dayInMs = 1000 * 60 * 60 * 24 //86400000ms
+      const dayDiff = (timeDiffInMs / dayInMs ).toFixed()
+      
+      return dayDiff
+    }
+  }
 }
-
-
-function 
 
 // page index
 routes.get('/', Job.controllers.index)
